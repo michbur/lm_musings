650373278ea79bcd911fad6f2d206dfbe2a2cc8b
michbur <michalburdukiewicz@gmail.com>
2016-06-01 12:29:54 +0200

    added as_dat and improved border search

--- simple_example.R
+++ simple_example.R
@@ -23,21 +23,60 @@ x <- x
 y <- y
 
 lm(y ~ x)
-mat <- model.matrix(m)
+
 lm.fit(mat, y)
 # coefficients
 
 
-coef_a <- .Call(stats:::C_Cdqrls, mat, y, 1e-07, FALSE)[["coefficients"]][2]
-coef_tmp <- coef_a
-
-# which way (positive or negative) changes the a?
-y_id <- 1
-y_multiplier <- 10
-while(coef_tmp > 0) {
+borderize_single <- function(mat, y, y_id, step) {
   y_tmp <- y
-  y_tmp[y_id] <- y_multiplier*y[y_id]
-  coef_tmp <- .Call(stats:::C_Cdqrls, mat, y_tmp, 1e-07, FALSE)[["coefficients"]][2]
-  y_multiplier <- y_multiplier + 1
-  print(coef_tmp)
+  y_tmp[y_id] <- 2*y[y_id]
+  coef_tmp <- .Call(stats:::C_Cdqrls, mat, y_tmp, 1e-07, FALSE)[["coefficients"]][2] 
+  coef_raw <- .Call(stats:::C_Cdqrls, mat, y, 1e-07, FALSE)[["coefficients"]][2]
+  
+  step_multiplier <- 1
+  
+  if(coef_raw - coef_tmp > 0) {
+    top = 1
+    while(coef_tmp > 0) {
+      y_tmp <- y
+      y_tmp[y_id] <- y_tmp[y_id] + step*step_multiplier
+      coef_tmp <- .Call(stats:::C_Cdqrls, mat, y_tmp, 1e-07, FALSE)[["coefficients"]][2]
+      step_multiplier <- step_multiplier + 1
+    }
+  } else {
+    top = 0
+    while(coef_tmp > 0) {
+      y_tmp <- y
+      y_tmp[y_id] <- y_tmp[y_id] - step*step_multiplier
+      coef_tmp <- .Call(stats:::C_Cdqrls, mat, y_tmp, 1e-07, FALSE)[["coefficients"]][2]
+      step_multiplier <- step_multiplier + 1
+    }
+  }
+  
+  c(y_border = y_tmp[y_id], top = top)
 }
+
+
+borderize <- function(mat, y, step)
+  t(sapply(1L:length(y), function(i) { 
+    borderize_single(mat = mat, y = y, y_id = i, step = step)
+  }))
+
+
+
+get_borders <- function(x, y, step = 50) {
+  mat <- model.matrix(m)
+  borders <- borderize(mat, y, step)
+  data.frame(x = x, y = y, borders)
+}
+
+plot_borders <- function(border_df)
+  ggplot(dat, aes(x = x, y = y)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE) +
+  geom_point(aes(x = x, y = y_border, color = factor(top))) +
+  geom_polygon(aes(x = x, y = y_border, fill = factor(top)))
+
+read.csv("as_dat.csv")
+  
